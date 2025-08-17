8ebe16f6bd5663a9ccbe08d6348b74dde09166c6
Gusti199410 <87740299+Gusti199410@users.noreply.github.com>
2025-08-16 21:27:01 -0300

    Segundo commit

--- arch_txt.h
+++ arch_txt.h
@@ -1,5 +1,10 @@
 #ifndef ARCH_TXT_H_INCLUDED
 #define ARCH_TXT_H_INCLUDED
+#include <stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+
 
 
 
--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include"arch_txt.h"
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
 }
